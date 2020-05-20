cd92ae096e23b5cd3c466c92fc461404936a8cc7
minjyo <minjyo8823@gmail.com>
2020-05-20 18:18:04 +0900

    junit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,7 +15,6 @@ class CalculatorTest {
     void subtract() {
         assertEquals(3, c.subtract(5, 2));
         assertEquals(3, c.subtract(5, 2));
-        assertEquals(3, c.subtract(5, 2));
     }
 
     @Test
